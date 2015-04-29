fa4ac7c227b0d75e2d5fac843f266e8c14243939
Nicolas Eckhart <nicolas.eckhart@renuo.ch>
2015-04-29 15:36:44 +0200

    added some rack tests

--- spec/api_keys_spec.rb
+++ spec/api_keys_spec.rb
@@ -1,7 +1,14 @@
 require File.dirname(__FILE__) + '/../models/api_keys.rb'
 require File.dirname(__FILE__) + '/../models/api_key.rb'
+require File.dirname(__FILE__) + '/../app.rb'
+require 'rack/test'
+
+def app
+  Sinatra::Application
+end
 
 RSpec.describe 'ApiKeys', type: :model do
+  include Rack::Test::Methods
   context 'create api_keys array from env vars' do
 
     let!(:api_keys) { ApiKeys.new('{"key":"12345678","app_name":"foobar","environment": "test"}') }
@@ -14,13 +21,23 @@ RSpec.describe 'ApiKeys', type: :model do
       expect(api_keys.api_keys.first.environment).to eq(test_key.environment)
     end
 
-    it 'check method should return false when inputting an unknown key' do
+    it 'sees if the check method return false if an unknown key is posted' do
       expect(api_keys.check('87654321')).to be_falsey
     end
 
-    it 'check method should return the api_key when inputting an known key' do
+    it 'sees if the the check method returns the api_key if a known key is posted' do
       expect(api_keys.check('12345678')).to be_truthy
       expect(api_keys.check('12345678').class).to be(ApiKey)
+    end
+
+    it 'checks that an ok status is returned when posting a know api key to /generate_policy' do
+      post '/generate_policy', params={api_key: '12345678'}
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'checks that a 403 status is returned when posting an unknown api key to /generate_policy' do
+      post '/generate_policy', params={api_key: 'foobar'}
+      expect(last_response.status).to eq(403)
     end
   end
 end
